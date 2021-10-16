1fed50f124ca14a1be87528d7cd9d192bc745b90
Eric Christensen <hebrew.8.12@gmail.com>
2021-10-15 20:25:36 -0700

    10/15/21 20:25

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/georgysavva/scany/pgxscan"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -13,11 +12,49 @@ import (
 var DBPool *pgxpool.Pool
 
 type Item struct {
-	ID        string
-	Product   string
-	Serial    string
-	Condition string
-	Year      string
+	ID        string `json:"id"`
+	Product   string `json:"product"`
+	Serial    string `json:"serial"`
+	Condition string `json:"condition"`
+	Year      string `json:"year"`
+}
+
+func getItems(db *pgxpool.Pool) ([]Item, error) {
+	var items []Item
+	err := pgxscan.Select(context.Background(), db, &items, "SELECT * FROM items")
+	if err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+func getItem(db *pgxpool.Pool, id string) (*Item, error) {
+	var item Item
+	err := pgxscan.Get(context.Background(), db, &item, "SELECT * FROM items WHERE id = $1", id)
+	if err != nil {
+		return nil, err
+	}
+	return &item, nil
+}
+
+func createItem(db *pgxpool.Pool, i Item) (*Item, error) {
+	var item Item
+	err := pgxscan.Get(context.Background(), db, &item, "INSERT INTO items (product, serial, condition, year) VALUES ($1, $2, $3, $4) returning *", &i.Product, &i.Serial, &i.Condition, &i.Year)
+	if err != nil {
+		return nil, err
+	}
+	return &item, nil
+}
+
+// func updateItem(db *pgxpool.Pool, id string) {}
+
+func deleteItem(db *pgxpool.Pool, id string) bool {
+	res, err := db.Exec(context.Background(), "DELETE FROM items WHERE id = $1", id)
+	if err != nil {
+		return false
+	}
+	count := res.RowsAffected()
+	return count != 0
 }
 
 func main() {
@@ -28,20 +65,38 @@ func main() {
 	}
 	defer DBPool.Close()
 
-	var item Item
-
-	start := time.Now()
-	for i := 0; i < 1489; i++ {
-
-		err = pgxscan.Get(context.Background(), DBPool, &item, "SELECT * FROM items WHERE id = $1", "696d9aa6-8b85-47f6-b92b-af8d34244c6d")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-			os.Exit(1)
-		}
-
+	_, err = DBPool.Exec(context.Background(), `CREATE TABLE IF NOT EXISTS ITEMS (
+		id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+		product VARCHAR(150) NOT NULL,
+		serial VARCHAR(150) NOT NULL,
+		condition VARCHAR(150) NOT NULL,
+		year VARCHAR(150) NOT NULL
+	)`)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
-	duration := time.Since(start)
-	fmt.Println(duration.Seconds())
 
-	// fmt.Println(item.ID)
+	itemTwo, err := createItem(DBPool, Item{Product: "Macbook Pro", Serial: "cc324132", Condition: "Good", Year: "2021"})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(*itemTwo)
+
+	itemThree, err := getItem(DBPool, itemTwo.ID)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(*itemThree)
+
+	itemsList, err := getItems(DBPool)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(len(itemsList))
+
+	deleteStatus := deleteItem(DBPool, itemTwo.ID)
+	fmt.Println(deleteStatus)
 }
